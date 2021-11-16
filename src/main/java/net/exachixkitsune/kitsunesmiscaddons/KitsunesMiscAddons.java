b2669fb48ad5c9605a4b0bf5d5e1be28f9978e70
exachixkitsune <exachixkitsune>
2021-11-16 15:59:11 +0000

    Version 1.1 - but actually changing version numbers

--- src/main/java/net/exachixkitsune/kitsunesmiscaddons/KitsunesMiscAddons.java
+++ src/main/java/net/exachixkitsune/kitsunesmiscaddons/KitsunesMiscAddons.java
@@ -35,7 +35,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class KitsunesMiscAddons {
 	public static final String MODID = "kitsunesmiscaddons";
     public static final String MODNAME = "Kitsunes Misc Addons";
-    public static final String VERSION = "1.16.5-1.0";
+    public static final String VERSION = "1.16.5-1.1";
     
     // Constructor
  	public KitsunesMiscAddons() {
